e038897814a601a1a71d46cca53410408e52d87c
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-01-21 11:12:01 +0100

    Ajout fonction main + flake8

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -1,21 +1,24 @@
 import sys
-print("=== Command Quest ===\n")
 
-len_sys = len(sys.argv)
-has_argument = True if len_sys > 1 else False
+def main() -> None:
+    print("=== Command Quest ===\n")
 
-if (has_argument is False):
-	print("No arguments provided!")
+    len_sys = len(sys.argv)
+    has_argument = True if len_sys > 1 else False
 
-print("Program name: ", sys.argv[0]) 
+    if (has_argument is False):
+        print("No arguments provided!")
 
-if (has_argument is True):
-	print(f"Arguments received: {len_sys - 1}")
-	i: int = 1
-	for arg in sys.argv[1:]:
-		print(f"Argument {i}: {arg}")
-		i += 1
+    print("Program name: ", sys.argv[0]) 
 
-print(f"Total arguments: {len_sys}\n")
-		
-# print("Argument List:", str(sys.argv))
+    if (has_argument is True):
+        print(f"Arguments received: {len_sys - 1}")
+        i: int = 1
+        for arg in sys.argv[1:]:
+            print(f"Argument {i}: {arg}")
+            i += 1
+
+    print(f"Total arguments: {len_sys}\n")
+        
+if __name__ == "__main__":
+    main()
